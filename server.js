f3e7ff09b3361d88af59a520f324ea55b0403058
hotreloadjohn <johnng87@protonmail.com>
2023-04-23 19:37:28 +0800

    feat: added cors

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+
+// config cors
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
